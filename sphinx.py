295e3ed7df8c57e2024493f6755d305c43129909
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2020-05-19 14:22:47 +0900

    push

--- sphinx.py
+++ sphinx.py
@@ -4,24 +4,20 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 fzf = FzfPrompt()
-
-basic_url = "http://tdoc.info/sphinx-reverse-dict/basic/"
-last_url = ".html"
-sphinx_name_list = ["section", "list", "sourcecode", "table", "image", "link"]
+sphinx_name_list = ["section", "list",
+                    "sourcecode", "table", "image", "link"]
 
 
 def generate_dict(sphinx_name_list):
+    basic_url = "http://tdoc.info/sphinx-reverse-dict/basic/"
+    last_url = ".html"
     sphinx_dict = {}
     for name in sphinx_name_list:
         sphinx_dict.update({name: basic_url + name + last_url})
     return sphinx_dict
 
 
-sphinx_dict = generate_dict(sphinx_name_list)
-name = fzf.prompt(sphinx_name_list)[0]
-
-
-def url_process():
+def url_process(sphinx_dict, name):
     document = ""
     url = urllib.request.Request(sphinx_dict[name])
     with urllib.request.urlopen(url) as res:
@@ -34,7 +30,9 @@ def url_process():
 
 
 def main():
-    url_process()
+    sphinx_dict = generate_dict(sphinx_name_list)
+    name = fzf.prompt(sphinx_name_list)[0]
+    url_process(sphinx_dict, name)
 
 
 main()
